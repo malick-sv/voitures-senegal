351d4df6558777b18cb43d2ead0a8ba8a13b44c9
Malick <malick@example.com>
2025-07-05 21:38:57 +0200

    Ajout du contenu du site

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Merci pour votre intérêt ! Nous vous contacterons bientôt.");
+  });
+});
+// Script vide pour l'instant
+console.log("Bienvenue sur Voitures Sénégal !");
